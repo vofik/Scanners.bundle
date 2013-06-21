027a8377794b67599e3a9bec7af0c3636bc20340
mmccurdy07 <mmccurdy@gmail.com>
2013-06-21 14:07:30 -0700

    Add .tp video file extension.

--- Contents/Resources/Common/VideoFiles.py
+++ Contents/Resources/Common/VideoFiles.py
@@ -3,7 +3,7 @@ import Filter
 import os.path, re, datetime, titlecase, unicodedata
 
 video_exts = ['3g2', '3gp', 'asf', 'asx', 'avc', 'avi', 'avs', 'bin', 'bivx', 'bup', 'divx', 'dv', 'dvr-ms', 'evo', 'fli', 'flv', 'ifo', 'img', 
-              'iso', 'm2t', 'm2ts', 'm2v', 'm4v', 'mkv', 'mov', 'mp4', 'mpeg', 'mpg', 'mts', 'nrg', 'nsv', 'nuv', 'ogm', 'ogv', 
+              'iso', 'm2t', 'm2ts', 'm2v', 'm4v', 'mkv', 'mov', 'mp4', 'mpeg', 'mpg', 'mts', 'nrg', 'nsv', 'nuv', 'ogm', 'ogv', 'tp',
               'pva', 'qt', 'rm', 'rmvb', 'sdp', 'svq3', 'strm', 'ts', 'ty', 'vdr', 'viv', 'vob', 'vp3', 'wmv', 'wpl', 'wtv', 'xsp', 'xvid', 'webm']
 
 ignore_samples = ['[-\._ ]sample', 'sample[-\._ ]']
